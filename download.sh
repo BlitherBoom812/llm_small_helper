bb8b4bf8df8aa2277a488c399c95e5e70fa2452e
Guoyun Tian <88619302+BlitherBoom812@users.noreply.github.com>
2024-10-15 21:55:02 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,6 +2,7 @@ export HF_ENDPOINT=https://hf-mirror.com
 export HF_CACHE=$HF_DIR
 export TRANSFORMERS_CACHE=$HF_DIR
 export HUGGINGFACE_HUB_CACHE=$HF_DIR
+export HF_HOME=$HF_DIR
 ORGS=ToolBench
 MODEL=ToolLLaMA-2-7b-v2
 huggingface-cli download --resume-download --local-dir-use-symlinks False $ORGS/$MODEL --local-dir $HUGGINGFACE_HUB_CACHE/$MODEL
